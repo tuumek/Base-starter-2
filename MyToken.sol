7dac491566e64845f70e3fa2b7899562816e015d
tuumek <136378271+tuumek@users.noreply.github.com>
2025-12-17 08:18:37 +0100

    Create MyToken contract with ERC20 implementation

---
+++ MyToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("My Base Token", "MBT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
